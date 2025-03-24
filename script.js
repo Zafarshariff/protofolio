7211cbded4bf83b07e0f9eb9ea157fcf6abd22aa
Jaffar Shariff <110933042+Zafarshariff@users.noreply.github.com>
2025-03-25 04:04:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ document.addEventListener("mousemove", (e) => {
     const core = document.querySelector(".cursor-core");
     const trail = document.querySelector(".cursor-trail");
 
-    // Position core instantly
     core.style.left = e.pageX + "px";
     core.style.top = e.pageY + "px";
-
-    // Trail follows with slight delay
     trail.style.left = e.pageX + "px";
     trail.style.top = e.pageY + "px";
-
     cursor.style.display = "block";
 });
 
